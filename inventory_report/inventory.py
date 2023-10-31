78b42e0ef9daa07802de83b5595782abff84d399
Lívio Lopes <antonio.lvio760@gmail.com>
2023-10-31 20:42:00 -0300

    feat(Inventory): implements class Inventory with decorator @dataclass

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from dataclasses import dataclass
+from inventory_report.product import Product
+
+
+@dataclass
 class Inventory:
-    pass
+    __data: list[Product] = list()
+
+    def add_data(self, data: Product) -> None:
+        self.__data.append(data)
+
+    @property
+    def data(self) -> list[Product]:
+        return self.__data
