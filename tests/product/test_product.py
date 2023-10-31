2a93526ab28f46fda4a6f143c2c92de7be554bee
Lívio Lopes <antonio.lvio760@gmail.com>
2023-10-31 15:24:49 -0300

    test(Product): test method __init__ from Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,4 +1,21 @@
-# Open PR
+from inventory_report.product import Product
+
 
 def test_create_product() -> None:
-    raise NotImplementedError
+    config = {
+        "id": "1",
+        "product_name": "boardgame",
+        "company_name": "hasbro",
+        "manufacturing_date": "2021-08-01",
+        "expiration_date": "2022-08-01",
+        "serial_number": "123456",
+        "storage_instructions": "keep in dry place",
+    }
+    instance = Product(**config)
+    assert instance.id == "1"
+    assert instance.product_name == "boardgame"
+    assert instance.company_name == "hasbro"
+    assert instance.manufacturing_date == "2021-08-01"
+    assert instance.expiration_date == "2022-08-01"
+    assert instance.serial_number == "123456"
+    assert instance.storage_instructions == "keep in dry place"
