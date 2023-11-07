afd6ce74dc2387338a9f9450033f5484f7592061
Lívio Lopes <antonio.lvio760@gmail.com>
2023-11-07 00:23:43 -0300

    feat(input_handler): Process the report given a list of file paths and a report type,     and returns the result

--- inventory_report/cli/input_handler.py
+++ inventory_report/cli/input_handler.py
@@ -1,4 +1,10 @@
 from typing import List
+from inventory_report.reports.complete_report import (
+    SimpleReport,
+    CompleteReport,
+)
+from inventory_report.inventory import Inventory
+from inventory_report.importers import JsonImporter, CsvImporter
 
 
 def process_report_request(file_paths: List[str], report_type: str) -> str:
@@ -6,4 +12,25 @@ def process_report_request(file_paths: List[str], report_type: str) -> str:
     Process the report given a list of file paths and a report type,
     and returns the result.
     """
-    raise NotImplementedError
+
+    inventories = []
+    if report_type == "simple":
+        report = SimpleReport()
+    elif report_type == "complete":
+        report = CompleteReport()
+    else:
+        raise ValueError("Report type is invalid.")
+
+    for path in file_paths:
+        if path.endswith("json"):
+            products = JsonImporter(path)
+        else:
+            products = CsvImporter(path)
+
+        inventory = Inventory(products.import_data())
+
+        inventories.append(inventory)
+
+    for inventory in inventories:
+        report.add_inventory(inventory)
+    return report.generate()
